d61972500eebb05996abdfc372488693839dedb6
nathanhonorato <nathanhonorato@hotmail.com>
2023-06-01 11:52:06 -0300

    adicionando "encontrar_pior_ano_time"

---
+++ R/brasileirao.R
@@ -0,0 +1,9 @@
+encontrar_pior_ano_time <- function(time) {
+  partidas_brasileirao |>
+    dplyr::group_by(temporada, quem_ganhou) |>
+    dplyr::filter(quem_ganhou != "Empate", quem_ganhou %in% time) |>
+    dplyr::count(quem_ganhou, sort = TRUE, name = 'n_vitorias') |>
+    dplyr::ungroup() |>
+    dplyr::filter(n_vitorias == min(n_vitorias))
+
+}
